4db5b3ae5401b6021bf25729a7a4f1bf1b44d6c5
xzw <xuzhiwei1@pconline.com.cn>
2023-09-07 09:57:26 +0800

    m

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,6 @@
-Parse.Cloud.define('hello', async (request) => {
-  const greeting = `Hello World!`;
-  return greeting;
+Parse.Cloud.define('getAllUser', async (request, response) => {
+  // console.log(Parse);
+  const form = request.params.form;
+  const query = new Parse.Query(form);
+  return query.find({ useMasterKey: true });
 });
